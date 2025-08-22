c93877997267a938c2274408c7705c07e34db3a9
Ameer Muavia Shah <mavee.shah@hotmail.com>
2025-08-22 20:58:51 +0500

    Added Migration Test Doctype

---
+++ doctype_migration_tool/doctype_migration_tool/doctype/leo_migration_test/leo_migration_test.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Ameer Muavia and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Leo Migration Test", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ doctype_migration_tool/doctype_migration_tool/doctype/leo_migration_test/test_leo_migration_test.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Ameer Muavia and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestLeoMigrationTest(FrappeTestCase):
+	pass
